e3be9dc717d058542bb95f76478f0cc0b4445007
NielsDev <niels@niels-mats.be>
2017-06-06 16:04:32 +0200

    Fix bug with char array initialization

--- CSharpLLVM/Generator/Instructions/Misc/EmitLdtoken.cs
+++ CSharpLLVM/Generator/Instructions/Misc/EmitLdtoken.cs
@@ -26,24 +26,38 @@ namespace CSharpLLVM.Generator.Instructions.Misc
                 FieldReference fieldRef = (FieldReference)token;
                 FieldDefinition fieldDef = fieldRef.Resolve();
 
-                // FIXME: we only support array initialization by "System.Runtime.CompilerServices.RuntimeHelpers.InitializeArray" currently
+                // We only support array initialization by "System.Runtime.CompilerServices.RuntimeHelpers.InitializeArray" currently
                 if (instruction.Next.OpCode.Code == Code.Call)
                 {
                     MethodReference methodRef = (MethodReference)instruction.Next.Operand;
                     if (methodRef.Name == "InitializeArray" && methodRef.DeclaringType.FullName == "System.Runtime.CompilerServices.RuntimeHelpers")
                     {
-                        // CIL creates this array as a byte[]
-                        ValueRef[] values = new ValueRef[fieldDef.InitialValue.Length];
-                        for (int i = 0; i < fieldDef.InitialValue.Length; i++)
-                            values[i] = LLVM.ConstInt(TypeHelper.Int8, fieldDef.InitialValue[i], false);
+                        // Note that the top value on the stack is currently the destination array
+                        // So that means if we have a char[] on the top, we need to interpret the array differently because we treat char as 8-bit...
+                        StackElement top = context.CurrentStack.Peek();
+                        bool isCharArray = (top.ILType.FullName == "System.Char[]");
+                        int dstArrayLength = (isCharArray) ? (fieldDef.InitialValue.Length / 2) : (fieldDef.InitialValue.Length);
+                        ValueRef[] values = new ValueRef[dstArrayLength];
 
-                        TypeRef globalType = LLVM.ArrayType(TypeHelper.Int8, (uint)fieldDef.InitialValue.Length);
+                        // CIL creates this array as a byte[]
+                        if (isCharArray)
+                        {
+                            for (int i = 0; i < dstArrayLength; i++)
+                                values[i] = LLVM.ConstInt(TypeHelper.Int8, fieldDef.InitialValue[i * 2], false);
+                        }
+                        else
+                        {
+                            for (int i = 0; i < dstArrayLength; i++)
+                                values[i] = LLVM.ConstInt(TypeHelper.Int8, fieldDef.InitialValue[i], false);
+                        }
+
+                        TypeRef globalType = LLVM.ArrayType(TypeHelper.Int8, (uint)dstArrayLength);
                         ValueRef global = LLVM.AddGlobal(context.Compiler.Module, globalType, "initarray");
                         LLVM.SetInitializer(global, LLVM.ConstArray(TypeHelper.Int8, values));
 
                         // Push the reference and the size
                         context.CurrentStack.Push(new StackElement(global, null, globalType));
-                        ValueRef size = LLVM.ConstInt(TypeHelper.NativeIntType, (ulong)fieldDef.InitialValue.Length, false);
+                        ValueRef size = LLVM.ConstInt(TypeHelper.NativeIntType, (ulong)dstArrayLength, false);
                         context.CurrentStack.Push(new StackElement(size, null, TypeHelper.NativeIntType));
                     }
                     else
