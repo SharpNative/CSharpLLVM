7041eae365758ece6b1471c99041a8866b3d714a
NielsDev <niels@niels-mats.be>
2017-06-06 20:19:58 +0200

    Support sizeof

---
+++ CSharpLLVM/Generator/Instructions/Misc/EmitSizeof.cs
@@ -0,0 +1,27 @@
+﻿using Swigged.LLVM;
+using CSharpLLVM.Compilation;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using CSharpLLVM.Helpers;
+using CSharpLLVM.Stack;
+
+namespace CSharpLLVM.Generator.Instructions.Misc
+{
+    [InstructionHandler(Code.Sizeof)]
+    class EmitSizeof : ICodeEmitter
+    {
+        /// <summary>
+        /// Emits a sizeof instruction
+        /// </summary>
+        /// <param name="instruction">The instruction</param>
+        /// <param name="context">The context</param>
+        /// <param name="builder">The builder</param>
+        public void Emit(Instruction instruction, MethodContext context, BuilderRef builder)
+        {
+            TypeDefinition type = (TypeDefinition)instruction.Operand;
+            TypeRef typeRef = TypeHelper.GetTypeRefFromType(type);
+            ulong size = LLVM.SizeOfTypeInBits(context.Compiler.TargetData, typeRef) / 8;
+            context.CurrentStack.Push(new StackElement(LLVM.ConstInt(TypeHelper.Int32, size, false), typeof(int).GetTypeReference(context.Compiler), TypeHelper.Int32));
+        }
+    }
+}
