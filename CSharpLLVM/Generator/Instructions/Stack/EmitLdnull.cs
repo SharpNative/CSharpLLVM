fb40301dfdb0eb590cb68a27308fc01a1a76beec
NielsDev <niels@niels-mats.be>
2017-06-06 13:47:46 +0200

    Add ldnull

---
+++ CSharpLLVM/Generator/Instructions/Stack/EmitLdnull.cs
@@ -0,0 +1,23 @@
+﻿using Swigged.LLVM;
+using CSharpLLVM.Compilation;
+using Mono.Cecil.Cil;
+using CSharpLLVM.Stack;
+using CSharpLLVM.Helpers;
+
+namespace CSharpLLVM.Generator.Instructions.Stack
+{
+    [InstructionHandler(Code.Ldnull)]
+    class EmitLdnull : ICodeEmitter
+    {
+        /// <summary>
+        /// Emits a ldnull instruction
+        /// </summary>
+        /// <param name="instruction">The instruction</param>
+        /// <param name="context">The context</param>
+        /// <param name="builder">The builder</param>
+        public void Emit(Instruction instruction, MethodContext context, BuilderRef builder)
+        {
+            context.CurrentStack.Push(new StackElement(LLVM.ConstNull(TypeHelper.VoidPtr), typeof(object).GetTypeReference(context.Compiler), TypeHelper.VoidPtr));
+        }
+    }
+}
